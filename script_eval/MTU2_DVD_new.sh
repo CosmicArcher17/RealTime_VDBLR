1e4a5a96ebcf50ee653d1e64cd7d7cd5a22efab9
Aadit_17 <148260267+CosmicArcher17@users.noreply.github.com>
2025-04-08 16:30:03 +0530

    Update MTU2_DVD_new.sh

--- script_eval/MTU2_DVD_new.sh
+++ script_eval/MTU2_DVD_new.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+
+py3clean ./
+
+CUDA_VISIBLE_DEVICES=0 python run.py \
+    --config config_MTU2 \
+    --mode MTU2_DVD \
+    --network MTU \
+    --trainer trainer_multi_opt \
+    --data DVD \
+    --ckpt_abs_name ckpt/MTU2_DVD.pytorch \
+    --eval_mode new_eval \
+    --is_quan \
+    --is_qual
